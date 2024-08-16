246d03bd1c06880e8db2ff705b3eba8478085fd9
muradov-b <133116277+Uzum-B01@users.noreply.github.com>
2024-08-16 12:25:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ function calculate() {
   var workedHours = parseFloat(document.getElementById("workedHours").value) || 0;
   var medical = parseFloat(document.getElementById("medical").value) || 0;
   var KPI = parseFloat(document.getElementById("KPI").value) || 0;
+  var pererab = parseFloat(document.getElementById("pererab").value) || 0;
+
 
   var oklad = 3308880
   var salary = 2202200; // Установите значение переменной salaryRate
@@ -40,9 +42,10 @@ function calculate() {
     okladCost = oklad/normHours*workedHours
   }
 
-  if (workedHours > normHours){
-    okladCost = oklad
-perer = oklad/normHours*(workedHours-normHours)
+
+
+  if(pererab > 0){
+perer =  roundUpToNearestThousand(oklad/normHours*pererab)
   }
 
   if (medical > 0) {
@@ -53,7 +56,7 @@ perer = oklad/normHours*(workedHours-normHours)
         salaryCost = salary * KPI / 100;
   }
 
-  if (normHours > workedHours) {
+  if (normHours >= workedHours) {
     salaryCost = roundUpToNearestThousand(
       salary * KPI/normHours*workedHours/ 100
     );
@@ -62,17 +65,35 @@ perer = oklad/normHours*(workedHours-normHours)
   if (medical > 0){
    medCompensatition = oklad / normHours * medicalHours * 0.6
   }
+
+
+var  text1 = "Заполните все поля"
+var textColor = "black"
+
+if(workedHours >0 && normHours>0 && workedHours <= normHours){
+ text1 = "Оклад: " + formatNumber(roundUpToNearestThousand(okladCost))
+}
+if (workedHours > normHours){
+  text1 = "Кол-во отработанных часов не может быть больше нормы, сверх нормы укажите в строке переработки"
+  textColor = "red"
+}
+
+
 var avans = okladCost/2
 var zarplata = okladCost/2 + perer + medCompensatition + salaryCost
 var salaryTotal = okladCost + perer + medCompensatition + salaryCost
 
-  var resultsText = "Заполните все поля" +"<br>" + "Оклад: " + formatNumber(roundUpToNearestThousand(okladCost)) + 
-  "<br> Переработки: " + formatNumber(roundUpToNearestThousand(perer));
+  var resultsText = text1 ;
+
   var resultsText2 = "Заполните поля"
-  +"<br>" + "Больничные: " + formatNumber(roundUpToNearestThousand(medCompensatition))
-   + "<br>" + "Выполнение KPI: " + toPercentage(KPI/100) 
+  +"<br>" + "Больничные: " + formatNumber(roundUpToNearestThousand(medCompensatition)) + 
+  "<br> Переработки: " + formatNumber(roundUpToNearestThousand(perer));
+
+  var resultsText3 = "Заполните поля" +
+   "<br>" + "Выполнение KPI: " + toPercentage(KPI/100) 
   +"<br>" + "Сумма бонусов: " + formatNumber(roundUpToNearestThousand(salaryCost));
-  var totalText = "Оклад: " + formatNumber(roundUpToNearestThousand(okladCost)) +
+
+  var totalText = text1 +
   "<br> Переработки: " + formatNumber(roundUpToNearestThousand(perer)) +
   "<br> KPI: " + formatNumber(roundUpToNearestThousand(salaryCost)) +
   "<br> Больничные: " + formatNumber(roundUpToNearestThousand(medCompensatition)) +
@@ -88,6 +109,7 @@ var salaryTotal = okladCost + perer + medCompensatition + salaryCost
 
   updateResults(resultsText);
   updateResults2(resultsText2);
+  updateResults3(resultsText3);
   updateTotalText(totalText);
 }
 
@@ -97,6 +119,10 @@ function updateResults(text) {
 
 function updateResults2(text) {
   document.getElementById("results2").innerHTML = text;
+}
+
+function updateResults3(text) {
+  document.getElementById("results3").innerHTML = text;
 }
 
 function updateTotalText(text) {
